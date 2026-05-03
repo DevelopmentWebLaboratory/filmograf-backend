468b02f4b3979da8c2b4c4209d8f0fd723560f4a
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2026-05-03 12:25:37 +0300

    fix (SearchIndexerService): disable api and port binding

--- backend/SearchIndexerService/Program.cs
+++ backend/SearchIndexerService/Program.cs
@@ -1,7 +1,6 @@
 using Filmograf.BaseLibrary.Util;
 using Filmograf.SearchIndexerService.Extendions;
 using Filmograf.SearchIndexerService.Util;
-using Filmograf.SearchIndexerService.Services.Middlewares;
 
 namespace Filmograf.SearchIndexerService;
 
@@ -9,16 +8,11 @@ public class Program
 {
     public async static Task Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        var builder = Host.CreateApplicationBuilder(args);
 
         AppSettingsUtil.LoadAppSettingsData();
         LocalAppSettingsUtil.LoadAppSettingsData();
-        
-        // Add services to the container.
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        
+
         // Add AutoMapper
         builder.Services
             .AddAutoMapper(_ => { }, typeof(Program).Assembly)
@@ -27,23 +21,7 @@ public class Program
             .AddComponents()
             .AddElastic();
         
-        var app = builder.Build();
-        
-        // ловушка для ошибок
-        app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-        // Configure the HTTP request pipeline.
-        if (AppSettingsUtil.AppSettings.DevMode)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseCors("AllowAll"); // todo: в проде поменять
-        app.UseAuthentication();
-        app.UseAuthorization();
-        
-        app.MapControllers();
-        app.Run();
+        var host = builder.Build();
+        await host.RunAsync();
     }
 }
